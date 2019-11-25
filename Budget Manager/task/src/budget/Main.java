ccea95fc0576e37664215f90112e7b959bf9e951
ghUserMan <apatiy.kirov@yandex.ru>
2019-11-25 22:25:46 +0300

    after stage 3

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -9,9 +9,25 @@ import java.util.*;
 public class Main {
 
     private static double balance = 0;
-    private static Map<String, Double> map = new LinkedHashMap<>();
+    private static Map<String, Double> food = new LinkedHashMap<>();
+    private static Map<String, Double> clothes = new LinkedHashMap<>();
+    private static Map<String, Double> entertainment = new LinkedHashMap<>();
+    private static Map<String, Double> other = new LinkedHashMap<>();
+    private static Map<String, Double> fullMap = new LinkedHashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    private static List<Map<String, Double>> bucket = new ArrayList<>();
+
+    static {
+        bucket.add(food);
+        bucket.add(clothes);
+        bucket.add(entertainment);
+        bucket.add(other);
+        bucket.add(fullMap);
+    }
+
+    private static String[] labels = new String[] {"Food:", "Clothes:", "Entertainment:", "Other:", "All:"};
+
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         boolean play = true;
@@ -24,15 +40,19 @@ public class Main {
             switch (action) {
                 case 1:
                     addIncome(scanner);
+                    System.out.println();
                     break;
                 case 2:
                     addPurchase(scanner);
+                    System.out.println();
                     break;
                 case 3:
-                    list();
+                    showPurchases(scanner);
+                    System.out.println();
                     break;
                 case 4:
                     showBalance(scanner);
+                    System.out.println();
                     break;
                 case 0:
                     play = false;
@@ -40,28 +60,103 @@ public class Main {
                 default:
                     throw new IllegalArgumentException("Its only could be 0-4");
             }
-            System.out.println();
         }
 
         System.out.println("Bye!");
     }
 
     private static void addPurchase(Scanner scanner) {
-        System.out.println("Enter purchase name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter its price:");
-        Double value = Double.parseDouble(scanner.nextLine());
-        System.out.println("Purchase was added!");
-        map.put(name, value);
-        balance -= value;
+
+        while (true) {
+            System.out.println("Choose the type of purchase");
+            System.out.println("1) Food");
+            System.out.println("2) Clothes");
+            System.out.println("3) Entertainment");
+            System.out.println("4) Other");
+            System.out.println("5) Back");
+
+            int action = Integer.parseInt(scanner.nextLine());
+
+            if (action < 1 || action > 4) {
+                return;
+            }
+
+//            Map<String, Double> currentMap = bucket.get(action - 1);
+            Map<String, Double> currentMap = getRightMap(action);
+
+            System.out.println();
+            System.out.println("Enter purchase name:");
+            String name = scanner.nextLine();
+            System.out.println("Enter its price:");
+            Double value = Double.parseDouble(scanner.nextLine());
+            System.out.println("Purchase was added!");
+
+            fullMap.put(name, value);
+//            System.out.print("full ");
+//            fullMap.forEach((k,v) -> System.out.print(k + ":" + v + " "));
+            currentMap.put(name, value);
+
+            balance -= value;
+            System.out.println();
+        }
+
     }
 
-    private static void list() {
-        if (map.isEmpty()) {
-            System.out.println("Purchase list is empty");
-        } else {
-            map.forEach((k, v) -> System.out.println(k + " $" + v));
+    private static Map<String, Double> getRightMap(int action) {
+        switch (action) {
+            case 1 :
+                return food;
+            case 2 :
+                return clothes;
+            case 3 :
+                return entertainment;
+            case 4 :
+                return other;
+            case 5 :
+                return fullMap;
+            default:
+                throw new IllegalArgumentException("to much");
         }
+    }
+
+    private static void showPurchases(Scanner scanner) {
+
+        while (true) {
+
+            System.out.println("Choose the type of purchases");
+            System.out.println("1) Food");
+            System.out.println("2) Clothes");
+            System.out.println("3) Entertainment");
+            System.out.println("4) Other");
+            System.out.println("5) All");
+            System.out.println("6) Back");
+
+            int action = Integer.parseInt(scanner.nextLine());
+
+            if (action < 1 || action > 5) {
+                return;
+            }
+
+//            Map<String, Double> currentMap = bucket.get(action - 1);
+            Map<String, Double> currentMap = getRightMap(action);
+
+//            System.out.print("action" + action + " map: " );
+//            currentMap.forEach((k,v) -> System.out.print(k + ":" + v + " "));
+            System.out.println();
+            System.out.println(labels[action - 1]);
+            if (currentMap == null || currentMap.size() < 1) {
+                System.out.println("Purchase list is empty");
+            } else {
+                currentMap.forEach((k, v) -> System.out.println(k + " $" + v));
+            }
+            showTotalSum(currentMap);
+            System.out.println();
+        }
+    }
+
+    private static void showTotalSum(Map<String, Double> map) {
+        Double sum = map.values().stream().reduce(0.0, Double::sum);
+        System.out.println("Total sum: $" + sum);
     }
 
     private static void showBalance(Scanner scanner) {
@@ -91,7 +186,7 @@ public class Main {
         double total = 0;
         List<String> list = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
+        String line;
         while((line = br.readLine()) != null) {
             list.add(line);
             total += getPriceFromLine(line);
